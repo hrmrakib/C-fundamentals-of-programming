ba020868b1f99c9e58ddafdb3b51503245529e13
hrmrakib <hrmrakibs@gmail.com>
2024-12-29 07:25:23 +0600

    Operators in C

---
+++ operators/index.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+   int a = 21;
+   int b = 8;
+   
+   int sum = a + b;
+   int sub = a - b;
+   int multiple = a * b;
+   int division = a / b;
+  
+   printf("Sum: %d\n", sum);
+   printf("Sub: %d\n", sub);
+   printf("Multiple: %d\n", multiple);
+   printf("Division: %d\n", division);
+
+   return 0;
+}
