ab7fa22c3ac919b2ec9e06010055881d40a1f629
hrmrakib <hrmrakibs@gmail.com>
2024-12-29 07:14:40 +0600

    variables in C

---
+++ variables/index.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+   int a = 12;
+   float b = 34.245;
+   double c = 545.120;
+   char d = 'K';
+   
+   printf("%d\n%f\n%lf\n%c\n", a,b,c,d);
+   
+
+    return 0;
+}
