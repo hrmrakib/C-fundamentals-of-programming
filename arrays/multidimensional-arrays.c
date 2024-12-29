1d0215f9994ab0ac32883d8dc24877c5e7254ba2
hrmrakib <hrmrakibs@gmail.com>
2024-12-29 13:57:08 +0600

    access the all elements of a multidimensional array

--- arrays/multidimensional-arrays.c
+++ arrays/multidimensional-arrays.c
@@ -7,3 +7,20 @@ int main() {
     
     return 0;
 }
+
+
+// access the all elements of a multidimensional array
+#include <stdio.h>
+
+int main() {
+    int matrix[2][3] = { {1, 4, 2}, {3, 6, 8} };
+    int i, j;
+    
+    for(i = 0; i < 2; ++i) {
+        for(j = 0; j < 3; ++j) {
+            printf("%d\n", matrix[i][j]);
+        }
+    }
+    
+    return 0;
+}
