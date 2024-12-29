84aa7f4e36cbb570da12672887e1790965f83e4a
hrmrakib <hrmrakibs@gmail.com>
2024-12-29 12:51:14 +0600

    array and loop on the array

---
+++ arrays/index.c
@@ -0,0 +1,14 @@
+// loop throw the array
+#include <stdio.h>
+
+int main() {
+    int arr[] = {12, 34, 67, 98};
+    int i;
+    int length = sizeof(arr) / sizeof(arr[0]) - 1;
+    
+    for(i = 0; i <= length; ++i) {
+        printf("%d\n", arr[i]);
+    }
+
+    return 0;
+}
