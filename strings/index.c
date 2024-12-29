4daf80fc93efd22242e6433e1485b25eb71b2908
hrmrakib <hrmrakibs@gmail.com>
2024-12-29 14:19:39 +0600

    string in C

---
+++ strings/index.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    char txt[] = "Hello, Bangladesh!";
+    char txt2[] = "Love you \'Bangladesh\'";
+    char txt3[] = "This is \tC program";
+    
+    printf("%s\n", txt);
+    printf("%s\n", txt2);
+    printf("%s\n", txt3);
+    
+    return 0;
+}
