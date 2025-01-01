4163d6f6b9bd801a624e70a0cb77b42d93853da5
hrmrakib <hrmrakibs@gmail.com>
2025-01-01 10:24:24 +0600

    structures in C - two ways define and access

--- structures/index.c
+++ structures/index.c
@@ -9,11 +9,14 @@ struct student {
 int main() {
     struct student s1;
     
+    // One way
     s1.id = 1001;
     strcpy(s1.name, "Mahfuz Alam"); // Use strcpy to copy a string into the array
+    printf("Name: %s and id: %d\n", s1.name, s1.id);
     
-    printf("Student id is: %d\n", s1.id);
-    printf("Student name is: %s\n", s1.name); // Use %s for string output
+    // Another way
+    struct student s2 = {1002, "Asif Mahmud"};
+    printf("Name: %s and id: %d\n", s2.name, s2.id);
 
     return 0;
 }
