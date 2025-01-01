d34fc9f19a0756605438235c5885988e26d4e0d8
hrmrakib <hrmrakibs@gmail.com>
2025-01-01 15:28:50 +0600

    simple algorithm to find largest numbers

---
+++ algorithms/Algorithm 2 - Find the largest number among three numbers/index.c
@@ -0,0 +1,22 @@
+/*
+Step 1: Start
+Step 2: Declare variables a, b, and c
+Step 3: Check: 
+                if (a > b) {
+                    else if (a > c) {
+                        a is the largest
+
+                        else {
+                            c is the largest
+                        }
+                    } 
+                } else {
+                    if (b > c) {
+                        b is largest
+                    } else {
+                        c is largest
+                    }
+                }
+Step 4: Display the largest
+Step 5: Stop
+*/
