f5f2edc3d8191619c660a09da97ec4f9395463ca
hrmrakib <hrmrakibs@gmail.com>
2025-01-01 15:29:49 +0600

    solving to find largest

--- algorithms/Algorithm 2 - Find the largest number among three numbers/index.c
+++ algorithms/Algorithm 2 - Find the largest number among three numbers/index.c
@@ -20,3 +20,30 @@ Step 3: Check:
 Step 4: Display the largest
 Step 5: Stop
 */
+
+#include <stdio.h>
+
+int largest (int a, int b, int c) {
+    if (a > b) {
+        if (a > c) {
+            printf("%d", a);
+        } 
+        else {
+            printf("%d", c);
+        }
+    } 
+    else {
+        if (b > c) {
+            printf("%d", b);
+        } else {
+            printf("%d", c);
+        }
+    }
+}
+
+int main() {
+    
+    largest(10, 20, 38);
+
+    return 0;
+}
